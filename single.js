305ac70d5994bad372c65b580d7e61e1c05564da
MarieWager <maje0026@stud.kea.dk>
2025-01-31 08:27:56 +0100

    init_script

---
+++ single.js
@@ -0,0 +1,39 @@
+let productId = 1163;
+let set_1 = document.querySelector(".set_1");
+
+fetch(`"https://kea-alt-del.dk/t7/api/products/${productId}"`)
+  .then((response) => response.json())
+  .then((data) => {
+    set_1.innerHTML = `
+    <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="1163">
+
+            <div>
+                <p class="bold">MODEL NAME</p>
+                <p>${data.productdisplayname}</p>
+
+                <p class="bold">COLOR</p>
+                <p>${data.basecolour}</p>
+
+                <p class="bold">INVENTORY NUMBER</p>
+                <p>${productId}</p>
+
+                <p class="bold">BRANDS</p>
+                <p>${data.brandname}</p>
+            </div>
+
+            <div class="order">
+                <p>${data.productdisplayname}</p>
+                <p class="cat">${data.subcategory}</p>
+                <p class="article">${data.articletype}</p>
+
+                <p class="discount">${data.discount}</p>
+                <p class="soldout">${data.soldout}</p>
+                
+                <p>Price
+                <br>
+                ${data.price}
+                </p>
+
+                <div class="add">Add to basket</div>
+    </div> `;
+  });
