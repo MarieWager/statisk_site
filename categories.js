3f5692a9a7ed4c579154774bb795c7e32ef5f306
MarieWager <maje0026@stud.kea.dk>
2025-02-03 14:48:28 +0100

    categories_done

--- categories.js
+++ categories.js
@@ -8,7 +8,7 @@ function showList(categories) {
   console.log(categories);
   const markup = categories
     .map(
-      (category) => `<div class="clc"><a href="produktliste.html">
+      (category) => `<div class="clc"><a href="produktliste.html?category=${category.category}">
                     <p>${category.category}</p>
                 </a></div>`
     )
@@ -17,3 +17,14 @@ function showList(categories) {
   console.log(markup);
   listCategories.innerHTML = markup; /*HTML SKAL VÆRE MED STORT!!!*/
 }
+
+/********************/
+/*URL*/
+/*opretter "ny" url med den valgte værdi, i dette tilfælde category*/
+const myCategory = new URLSearchParams(window.location.search).get("category");
+
+/*viser hvilken categori det er i konsol*/
+console.log("category:", myCategory);
+
+/*viser værdien som tekst* husk at vælge den tekst-form (h2,h3,p) fra html'en*/
+document.querySelector("p").textContent = `${myCategory}`;
